a1f5511f3befdf4f369da386a5fd60db4b6b0585
Jitendra <jitendra8911@gmail.com>
2015-07-19 00:07:48 -0400

    Designed menu, buttons, labels and table background. Things yet to be implemented are mentioned in Notes, and ToDo

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Gopinath on 7/18/2015.
+ */
+
+function onReserveClick()
+{
+    document.getElementById("liHome").className="";
+}
+
+function onEditReservationClick()
+{
+    document.getElementById("liHome").className="";
+}
